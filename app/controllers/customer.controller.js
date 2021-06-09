12c660ffbbb0625c04044d14620430a1e8e13739
Manohar Choudary <manoharchoudaryv@gmail.com>
2021-06-09 19:17:05 +0530

    apis

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -3,8 +3,54 @@ const Customer = db.customer;
 
 exports.saveNewCustomer = (req,res)=>{
     let data = req.body;
-    console.log(data);
     let customer = new Customer();
-    console.log(customer);
-    res.send(customer);
+    for(let p in data){
+        customer[p]=data[p];
+      }
+    customer.save((err,response)=>{
+        if(err){
+            return res.status(500).send({message:err});
+        }
+        res.status(201).send({
+            message:"Customer created Successfully",
+            data:response
+        })
+    })
+}
+
+exports.getAllCustomers = (req,res)=>{
+    Customer.find((err,response)=>{
+        if(err){
+            return res.status(500).send({message:err});
+        }
+        res.status(200).send({
+            message:"Customers retrieved Successfully",
+            data:response
+        })
+    })
+}
+
+exports.updateCustomer = (req,res)=>{
+    let data = req.body;
+    Customer.updateOne({_id:data.id},data,(err,response)=>{
+        if(err){
+            return res.status(500).send({message:err});
+        }
+        res.status(201).send({
+            message:"Customer Updated Successfully",
+            data:response
+        })
+    })
+}
+
+exports.deleteCustomer = (req,res)=>{
+    Customer.deleteOne({_id:req.params.cid},(err,response)=>{
+        if(err){
+            return res.status(500).send({message:err});
+        }
+        res.status(201).send({
+            message:"Customer Deleted Successfully",
+            data:response
+        })
+    })
 }
--- app/routes/customer.route.js
+++ app/routes/customer.route.js
@@ -2,5 +2,7 @@ const controller = require("../controllers/customer.controller");
 
 module.exports = function(app){
     app.post("/api/customers/saveNewCustomer",controller.saveNewCustomer);
-
+    app.get("/api/customers/getAllCustomers",controller.getAllCustomers);
+    app.put("/api/customers/updateCustomer",controller.updateCustomer);
+    app.delete("/api/customers/deleteCustomer/:cid",controller.deleteCustomer);
 }
